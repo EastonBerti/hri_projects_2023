ee6f42d9902bc76f58b802e53cf0cb92e1e9d9db
EastonBerti <easton.berti@gmail.com>
2023-09-22 16:11:56 -0700

    week1 part 3

---
+++ week1/scripts/lineStop.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+speed = 1
+
+def callback(data):
+    rangerDan = data.ranges
+    x = 0
+    global speed
+    print(speed)
+    for i in rangerDan:
+        if i < 0.5:
+            print("Stop!")
+            speed = 0
+            print("Speed:" + str(speed))
+            rospy.signal_shutdown("Collision Imminent")
+            return 0
+        else:
+            randomElse = True
+
+def move():
+    # Starts a new node
+    rospy.init_node('robot_cleaner', anonymous=True)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+    
+    rospy.Subscriber("/base_scan", LaserScan, callback)
+    
+    
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0    
+    
+    while not rospy.is_shutdown():
+        vel_msg.linear.x = abs(speed)
+        for i in range(75000):
+            vel_msg.linear.x = abs(speed)
+            pub.publish(vel_msg)
+        vel_msg.linear.x = 0
+        pub.publish(vel_msg)
+        print("hello")
+    rospy.spin()
+if __name__ == '__main__':
+    try:
+        #Testing our function
+        move()
+    except rospy.ROSInterruptException: pass
